22c5f72d1a44e71a7a970e5c98baa25ba80763d0
Architrixs <architsingh12376@gmail.com>
2022-06-26 02:12:16 +0530

    Configure AutoMapper; Add Member, Photo Dtos; Update UserController

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -14,7 +14,7 @@ namespace API.Entities
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastActive { get; set; }
+        public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
@@ -23,9 +23,9 @@ namespace API.Entities
         public string City { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
